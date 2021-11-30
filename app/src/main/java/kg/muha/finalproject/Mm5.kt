5b3801c2b7268baec23ae5cbb4a670f1ff62a3e9
name <yuldashov_m@auca.kg>
2021-11-30 23:50:07 +0600

    8str

---
+++ app/src/main/java/kg/muha/finalproject/Mm5.kt
@@ -0,0 +1,11 @@
+package kg.muha.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mm5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mm5)
+    }
+}
